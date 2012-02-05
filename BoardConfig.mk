bb69f7245cd045dbf8500f8dff5291f6efaa4f0d
Jesse Straub <goodboy735@gmail.com>
2012-02-04 17:54:06 -0800

    Fix for atmel touchscreens - Confirmed working!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
 
+BOARD_USE_LEGACY_TOUCHSCREEN := true
 BOARD_USE_LEGACY_TRACKPAD := true
 
 ## TEST ZONE ##
