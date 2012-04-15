afa3f8db1ef8ca633bf63e30a8aa47edfea01fca
Jesse Straub <goodboy735@gmail.com>
2012-04-15 05:49:27 -0700

    Oops, forgot to put libcamera.so in liberty.mk (c6262e219e)

--- liberty.mk
+++ liberty.mk
@@ -138,7 +138,8 @@ PRODUCT_COPY_FILES += \
     device/htc/liberty/vold.fstab:system/etc/vold.fstab \
     device/common/gps/gps.conf_US:system/etc/gps.conf \
     vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
-    device/htc/liberty/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd
+    device/htc/liberty/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
+    device/htc/liberty/prebuilt/lib/libcamera.so:system/lib/libcamera.so
 
 # Kernel modules
 
