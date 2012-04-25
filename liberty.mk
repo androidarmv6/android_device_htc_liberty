38545bbdec94a85aae2b5287f12c3910fa133334
Jesse Straub <goodboy735@gmail.com>
2012-04-24 23:55:14 -0700

    Camera preview & memleak fixes from gladiac (thanks!)

--- liberty.mk
+++ liberty.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
     gps.liberty \
     libmm-omxcore \
     libOmxCore \
+    libstagefrighthw \
     hwcomposer.default \
     hwcomposer.msm7x27 \
     com.android.future.usb.accessory \
