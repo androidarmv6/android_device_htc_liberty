7ad79e1fb979ac5353263c7712f5698768e33446
Jesse Straub <goodboy735@gmail.com>
2012-08-28 02:51:06 -0700

    Move lots of stuff to msm7x27-common

--- liberty.mk
+++ liberty.mk
@@ -17,8 +17,6 @@
 
 DEVICE_PACKAGE_OVERLAYS := device/htc/liberty/overlay
 
-## (1) First, the most specific values, i.e. the aspects that are specific to GSM
-
 # Keylayouts
 PRODUCT_COPY_FILES += \
     device/htc/liberty/prebuilt/usr/keylayout/liberty-keypad.kl:system/usr/keylayout/liberty-keypad.kl \
@@ -36,61 +34,17 @@ PRODUCT_COPY_FILES += \
     device/htc/liberty/init.liberty.rc:root/init.liberty.rc \
     device/htc/liberty/ueventd.liberty.rc:root/ueventd.liberty.rc
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libhtc_ril.so \
-    ro.ril.enable.dtm=1 \
-    ro.ril.hsdpa.category=8 \
-    ro.ril.hsupa.category=5 \
-    ro.ril.disable.fd.plmn.prefix=23402,23410,23411 \
-    ro.ril.def.agps.mode=1 \
-    ro.ril.hsxpa=2 \
-    ro.ril.gprsclass=12 \
-    mobiledata.interfaces=rmnet0,rmnet1,rmnet2,gprs,ppp0 \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15 \
-    ro.sf.lcd_density=160 \
-    ro.opengles.version=131072 \
-    ro.telephony.ril.v3=signalstrength
-
 # Default network type.
 # 0 => WCDMA preferred.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=0
 
-# For emmc phone storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.phone_storage = 0
-
-
 # This is a 512MB device, so 32mb heapsize
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapsize=32m
 
-## (1) First, the most specific values, i.e. the aspects that are specific to GSM
-
-## (2) Also get non-open-source GSM-specific aspects if available
+## Get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/liberty/liberty-vendor.mk)
-
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
-PRODUCT_PROPERTY_OVERRIDES += \
-    settings.display.autobacklight=1 \
-    settings.display.brightness=143 \
-    persist.service.mount.playsnd=0 \
-    ro.com.google.locationfeatures=1 \
-    ro.setupwizard.mode=OPTIONAL \
-    ro.setupwizard.enable_bypass=1 \
-    ro.media.dec.aud.wma.enabled=1 \
-    ro.media.dec.vid.wmv.enabled=1 \
-    dalvik.vm.dexopt-flags=m=y \
-    ro.config.sync=yes \
-    persist.sys.usb.config=mass_storage,adb \
-    dalvik.vm.dexopt-data-only=1 \
-    debug.sf.hw=1 \
-    debug.qctwa.statusbar=1 \
-    debug.qctwa.preservebuf=1 \
-    com.qc.hardware=true \
-    com.qc.hdmi_out=false \
-    debug.camcorder.disablemeta=1
 
 # Override /proc/sys/vm/dirty_ratio on UMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -101,9 +55,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 PRODUCT_PACKAGES += \
-    camera.msm7x27 \
     gps.liberty \
-    lights.liberty \
     sensors.liberty
 
 PRODUCT_COPY_FILES += \
@@ -112,8 +64,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
     device/htc/liberty/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
     device/htc/liberty/prebuilt/lib/libcamera.so:system/lib/libcamera.so \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf \
-    device/htc/liberty/audio_policy.conf:system/etc/audio_policy.conf
+    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
 
 # Copy prebuilt wifi module when it isn't being built with the kernel
 ifneq ($(FULL_KERNEL_BUILD),true)
